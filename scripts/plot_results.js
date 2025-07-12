d972cc5fc936f771265e3a58bac5369e9f0b2ae8
Alexander Jonathan Westley <24814245@sun.ac.za>
2025-07-13 01:06:26 +0200

    Save data

--- scripts/plot_results.js
+++ scripts/plot_results.js
@@ -1,19 +1,29 @@
 var myChart = null;
+var data = null
 
 export function plotCounts(counts) {
     const ctx = document.getElementById('barChart');
+
+    data = counts;
     
     if (myChart) {
         myChart.destroy();
+    }
+
+    if (data) {
+        document.getElementById('saveResultsBtn').disabled = false;
+    } else {
+        document.getElementById('saveResultsBtn').disabled = true;
+        return;
     }
     
     myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-        labels: Object.keys(counts),
+        labels: Object.keys(data),
         datasets: [{
             label: '# of Measurements',
-            data: Object.values(counts),
+            data: Object.values(data),
         }]
         },
         options: {
@@ -37,3 +47,23 @@ export function plotCounts(counts) {
         }
     });
 }
+
+document.getElementById('saveResultsBtn').addEventListener('click', function() {
+    if (!data) {
+        console.error('No data to save');
+        return;
+    }
+
+    // row 1 = all keys, row 2 = all values
+    const csvContent = "data:text/csv;charset=utf-8," 
+        + Object.keys(data).join(",") + "\n"
+        + Object.values(data).join(",");
+
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "measurement_counts.csv");
+    document.body.appendChild(link); // Required for FF
+
+    link.click();
+});
